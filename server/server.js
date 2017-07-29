f894830a08f01c0eebc62777331373990bb3a807
Alexander Hoyland <alex.hoyland@adventure-buddy.com>
2017-07-29 17:15:19 -0400

    Fixed up some stuff for nginx to serve permalinks to angular routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Coming soon!')
+})
+
 app.get('/node', function (req, res) {
   res.send('Alex loves Storcy!')
 })
