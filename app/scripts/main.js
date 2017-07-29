6afa66ef45ef0f3e7ae251d1fdf612fb3532e1e3
Alexander Hoyland <alex.hoyland@adventure-buddy.com>
2017-07-29 13:04:11 -0400

    Got scaffold working.  Fixed carousel.  Added terms and privacy pages. Now to give them all content!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,53 @@
-/**
- * Main AngularJS Web Application
- */
+// Use strict or jshint will bitch at us ======================================
+
+'use strict';
+
+// Main AngularJS Web Application =============================================
+ 
 var app = angular.module('adventureBuddyApp', ['ngRoute']);
 
-/**
- * Configure the Routes
- */
-app.config(['$routeProvider', function ($routeProvider) {
+// Configure the Routes =======================================================
+ 
+app.config(function ($routeProvider, $locationProvider) {
+	
+	// Make the location provider not randomly insert %2F in the URL
+    $locationProvider.hashPrefix('');
+	
+	//Set up the routes
     $routeProvider
     // Home
-    .when("/", {templateUrl: "views/home.html", controller: "PageCtrl"})
+    .when('/', {templateUrl: 'views/home.html', controller: 'PageCtrl'})
     // Pages
-    .when("/about", {templateUrl: "views/about.html", controller: "PageCtrl"})
-    .when("/gallery", {templateUrl: "views/gallery.html", controller: "PageCtrl"})
-    // // else 404
-    .otherwise("/404", {templateUrl: "404.html", controller: "PageCtrl"});
-}]);
+    .when('/about', {templateUrl: 'views/about.html', controller: 'PageCtrl'})
+    .when('/gallery', {templateUrl: 'views/gallery.html', controller: 'PageCtrl'})
+    .when('/tos', {templateUrl: 'views/terms.html', controller: 'PageCtrl'})
+    .when('/privacy', {templateUrl: 'views/privacy.html', controller: 'PageCtrl'})
+    // else 404
+    .otherwise('/404', {templateUrl: '404.html', controller: 'PageCtrl'});
+});
+
+// Controls all other Pages ===================================================
+ 
+app.controller('PageCtrl', function ($scope, $location, $http) {
+  console.log('Page Controller reporting for duty.\nScope is:\n%s\nLocation is:\n%sHttp is:\n%s\n\n',$scope, $location, $http);
+});
+
+// Carousel handler ===========================================================
 
 /**
- *Controls all other Pages
+ * This will intercept the right and left click icons on the carousel and
+ * call the bootstrap handler before the angular route provider gets it.
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-});
+
+var handle_nav = function(e) {
+	e.preventDefault();
+	var nav = this;
+	nav.parents('.carousel').carousel(nav.data('slide'));
+};
+
+$('.carousel').carousel({
+			interval: 5000,
+			pause: "hover",
+			wrap: true
+		})
+		.on('click', '.carousel-control', handle_nav);
